d6d4c75c46161103a06333da4b138b4e46e99187
Kristian Zondervan <kristian.zondervan@gmail.com>
2012-02-04 02:05:28 +0100

    abstracted collection response

--- Response/ListResultsResponse.php
+++ Response/ListResultsResponse.php
@@ -10,8 +10,6 @@
 
 namespace Netvlies\Bundle\BolOpenApiBundle\Response;
 
-// @todo refactor intro abstract class and specific classes for list and search
-class ListResultsResponse extends SearchResultsResponse
+class ListResultsResponse extends AbstractCollectionResponse
 {
-
 }
