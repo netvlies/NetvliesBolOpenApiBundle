728778feead6d07cec29eb35404ae7d64ac8687e
Kristian Zondervan <kristian.zondervan@gmail.com>
2012-02-10 11:12:36 +0100

    prepared travis-ci

---
+++ Tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the NetvliesBolOpenApiBundle.
+ *
+ * (c) Netvlies Internetdiensten
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file was copied from the FOSTwitterBundle package. Thanks Friends of Symfony.
+ */
+
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
